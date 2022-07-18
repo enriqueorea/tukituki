e95fe42512f1a18d665321e9fa99a4b2bc2bdff7
Enrique Orea <orea_96@hotmail.com>
2022-07-17 23:22:45 -0500

    Initial commit with navbar in progress atm

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import { useState, useEffect } from "react";
+import type { AppProps } from "next/app";
+import { Navbar, Sidebar } from "../components";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [isSSR, setIsSSR] = useState(true);
 
-export default MyApp
+  useEffect(() => {
+    setIsSSR(false);
+  }, []);
+
+  if (isSSR) return null;
+
+  return (
+    <div>
+      <Navbar />
+      <div className="flex gap-6 md:gap-20">
+        <div className="h-[92vh] overflow-hidden xl:overflow-auto">
+          <Sidebar />
+        </div>
+        <div className="mt-4 flex flex-col overflow-auto gap-10 flex-1 h-[88vh] videos">
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyApp;
